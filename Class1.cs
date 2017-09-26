7673392c5c02ef699b89098557e71b2cb5c9ada5
nikitamca <parmarnikita119@gmail.com>
2017-09-26 21:55:51 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace OMS.Models
+{
+    public class Class1
+    {
+        [Key]
+        public int eid { get; set; }
+
+        [Required(ErrorMessage = "Name not be blank.")]
+        public string ename { get; set; }
+        [Required(ErrorMessage = "Address not be blank.")]
+        public string address { get; set; }
+        [Required(ErrorMessage = "Email not be blank.")]
+        public string email { get; set; }
+        [Required(ErrorMessage = "Password not be blank.")]
+        public string password { get; set; }
+        [Required(ErrorMessage = "Confirm password not be blank.")]
+        public string cpassword { get; set; }
+        [Required(ErrorMessage="Contact number not be blank.")]
+        public int mono { get; set; }
+    }
+}
